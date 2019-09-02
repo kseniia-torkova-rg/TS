c18602702d4e4abf089e0db71241e8102d0f6bff
ksenya torkova <ksenya.torkova.stratis@gmail.com>
2019-09-02 15:03:35 +0300

    initial commit

---
+++ official-examples/greeter.js
@@ -0,0 +1,28 @@
+// function greeter(person: string) {
+//     return "Hello, " + person;
+// }
+// let user = "Jane User";
+// document.body.textContent = greeter(user);
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+// function greeter(person: Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName;
+// }
+// let user = { firstName: "Jane", lastName: "User" };
+// document.body.textContent = greeter(user);
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.middleInitial + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+document.body.textContent = greeter(user);
---
+++ official-examples/greeter.ts
@@ -0,0 +1,34 @@
+// function greeter(person: string) {
+//     return "Hello, " + person;
+// }
+// let user = "Jane User";
+// document.body.textContent = greeter(user);
+
+
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+// function greeter(person: Person) {
+//     return "Hello, " + person.firstName + " " + person.lastName;
+// }
+// let user = { firstName: "Jane", lastName: "User" };
+// document.body.textContent = greeter(user);
+
+
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+interface Person {
+    firstName: string;
+    middleInitial: string;
+    lastName: string;
+}
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.middleInitial + " " + person.lastName;
+}
+let user = new Student("Jane", "M.", "User");
+document.body.textContent = greeter(user);
