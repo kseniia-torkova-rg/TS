45a95725dd1db9b57c7f630b1f03bf23bfe4f00f
ksenya torkova <ksenya.torkova.stratis@gmail.com>
2019-09-06 18:57:51 +0300

    rename folder for string's template

---
+++ template-literals/template-literals-ts.js
@@ -0,0 +1,3 @@
+var personName = 'Ann';
+var greeting = "\n  This is a heading!\n  My greetings to you, " + personName + "\n";
+console.log(greeting);
---
+++ template-literals/template-literals-ts.ts
@@ -0,0 +1,6 @@
+const personName: string = 'Ann';
+const greeting: string = `
+  This is a heading!
+  My greetings to you, ${personName}
+`;
+console.log(greeting);
